53687819b7c2b11df1e03e46cbbb0c319871c928
Takurus <developer@takurus.com>
2022-06-28 20:45:40 -0400

    Initial Commit

---
+++ Super Sleep/Super_SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_SleepApp.swift
+//  Super Sleep
+//
+//  Created by Jack Farray on 2022-06-28.
+//
+
+import SwiftUI
+
+@main
+struct Super_SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
